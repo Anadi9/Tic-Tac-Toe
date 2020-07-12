7543d1e2af6c69bf5f49d3431d45ba5241dedb46
Anadi9 <anadithakur99@gmail.com>
2020-07-12 20:59:08 +0530

    Updates

---
+++ precache-manifest.9c537634d18bd718add6f53f74ccc4e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2bf2b5e3cd715471ae25601e50b7ea",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "dd326b6f6e2b408b9fea",
+    "url": "/tic-tac-toe/static/css/main.777c1f6c.chunk.css"
+  },
+  {
+    "revision": "333515a4d31e42dba7dd",
+    "url": "/tic-tac-toe/static/js/2.d51d0453.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.d51d0453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd326b6f6e2b408b9fea",
+    "url": "/tic-tac-toe/static/js/main.b4396f18.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
